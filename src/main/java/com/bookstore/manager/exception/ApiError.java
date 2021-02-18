d6ad42943c280a5e5014697e6ca325bff0109998
Restyled.io <commits@restyled.io>
2021-02-18 19:50:09 +0000

    Restyled by clang-format

--- src/main/java/com/bookstore/manager/exception/ApiError.java
+++ src/main/java/com/bookstore/manager/exception/ApiError.java
@@ -1,27 +1,26 @@
 package com.bookstore.manager.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiError {
-    private int code;
+  private int code;
 
-    private String status;
+  private String status;
 
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
-    private LocalDate timestamp;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
+  private LocalDate timestamp;
 
-    private String message;
+  private String message;
 
-    private List<String> errors;
+  private List<String> errors;
 }
--- src/main/java/com/bookstore/manager/exception/BookstoreExceptionHandler.java
+++ src/main/java/com/bookstore/manager/exception/BookstoreExceptionHandler.java
@@ -1,5 +1,14 @@
 package com.bookstore.manager.exception;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
@@ -14,48 +23,62 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.persistence.EntityExistsException;
-import javax.persistence.EntityNotFoundException;
-import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
 @ControllerAdvice
 public class BookstoreExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private ResponseEntity<Object> buildResponseEntity(HttpStatus httpStatus, String message, List<String> errors){
-        ApiError apiError = ApiError.builder()
-                .code(httpStatus.value())
-                .status(httpStatus.getReasonPhrase())
-                .message(message)
-                .errors(errors)
-                .timestamp(LocalDate.from(LocalDateTime.now()))
-                .build();
-        return ResponseEntity.status(httpStatus).body(apiError);
-    }
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Object> handlerEntityNotFoundException(EntityNotFoundException exception){
-        return buildResponseEntity(HttpStatus.NOT_FOUND, exception.getMessage(), Collections.singletonList(exception.getMessage()));
-    }
-    @ExceptionHandler(EntityExistsException.class)
-    public ResponseEntity<Object> handlerEntityExistException(EntityExistsException exception){
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, exception.getMessage(), Collections.singletonList(exception.getMessage()));
-    }
+  private ResponseEntity<Object> buildResponseEntity(HttpStatus httpStatus,
+                                                     String message,
+                                                     List<String> errors) {
+    ApiError apiError = ApiError.builder()
+                            .code(httpStatus.value())
+                            .status(httpStatus.getReasonPhrase())
+                            .message(message)
+                            .errors(errors)
+                            .timestamp(LocalDate.from(LocalDateTime.now()))
+                            .build();
+    return ResponseEntity.status(httpStatus).body(apiError);
+  }
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<Object>
+  handlerEntityNotFoundException(EntityNotFoundException exception) {
+    return buildResponseEntity(
+        HttpStatus.NOT_FOUND, exception.getMessage(),
+        Collections.singletonList(exception.getMessage()));
+  }
+  @ExceptionHandler(EntityExistsException.class)
+  public ResponseEntity<Object>
+  handlerEntityExistException(EntityExistsException exception) {
+    return buildResponseEntity(
+        HttpStatus.BAD_REQUEST, exception.getMessage(),
+        Collections.singletonList(exception.getMessage()));
+  }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, "Malformed JSON body and/or field error", Collections.singletonList(exception.getLocalizedMessage()));
-    }
+  @Override
+  protected ResponseEntity<Object>
+  handleHttpMessageNotReadable(HttpMessageNotReadableException exception,
+                               HttpHeaders headers, HttpStatus status,
+                               WebRequest request) {
+    return buildResponseEntity(
+        HttpStatus.BAD_REQUEST, "Malformed JSON body and/or field error",
+        Collections.singletonList(exception.getLocalizedMessage()));
+  }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        List<String> errors = new LinkedList<>();
-        exception.getBindingResult().getFieldErrors().forEach(x-> errors.add("Field "+x.getField().toUpperCase() + " " + x.getDefaultMessage()));
-        exception.getBindingResult().getAllErrors().forEach(y-> errors.add("Object "+y.getObjectName() + " " + y.getDefaultMessage()));
-        return buildResponseEntity(HttpStatus.BAD_REQUEST, "Informed argument(s) validation error(s)", errors);
-    }
+  @Override
+  protected ResponseEntity<Object>
+  handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+                               HttpHeaders headers, HttpStatus status,
+                               WebRequest request) {
+    List<String> errors = new LinkedList<>();
+    exception.getBindingResult().getFieldErrors().forEach(
+        x
+        -> errors.add("Field " + x.getField().toUpperCase() + " " +
+                      x.getDefaultMessage()));
+    exception.getBindingResult().getAllErrors().forEach(
+        y
+        -> errors.add("Object " + y.getObjectName() + " " +
+                      y.getDefaultMessage()));
+    return buildResponseEntity(HttpStatus.BAD_REQUEST,
+                               "Informed argument(s) validation error(s)",
+                               errors);
+  }
 }
