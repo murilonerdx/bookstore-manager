beb6eec1975a0e1d4ee4f27a7b1a9c735318264e
Restyled.io <commits@restyled.io>
2021-08-12 16:38:39 +0000

    Restyled by clang-format

--- src/main/java/com/murilonerdx/bookmanager/model/book/repository/BookRepository.java
+++ src/main/java/com/murilonerdx/bookmanager/model/book/repository/BookRepository.java
@@ -3,5 +3,4 @@ package com.murilonerdx.bookmanager.model.book.repository;
 import com.murilonerdx.bookmanager.model.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
+public interface BookRepository extends JpaRepository<Book, Long> {}
--- src/main/java/com/murilonerdx/bookmanager/model/publisher/repository/PublisherRepository.java
+++ src/main/java/com/murilonerdx/bookmanager/model/publisher/repository/PublisherRepository.java
@@ -3,5 +3,4 @@ package com.murilonerdx.bookmanager.model.publisher.repository;
 import com.murilonerdx.bookmanager.model.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PublisherRepository extends JpaRepository<User, Long> {
-}
+public interface PublisherRepository extends JpaRepository<User, Long> {}
