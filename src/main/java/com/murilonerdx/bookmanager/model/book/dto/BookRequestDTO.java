31ef153fdc6757484d33c7d29913c9a16894c136
Restyled.io <commits@restyled.io>
2021-08-12 17:11:33 +0000

    Restyled by clang-format

--- src/main/java/com/murilonerdx/bookmanager/model/book/dto/BookRequestDTO.java
+++ src/main/java/com/murilonerdx/bookmanager/model/book/dto/BookRequestDTO.java
@@ -1,42 +1,30 @@
 package com.murilonerdx.bookmanager.model.book.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.ISBN;
-
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookRequestDTO {
 
-    private Long id;
+  private Long id;
 
-    @NotNull
-    @NotEmpty
-    @Size(max = 255)
-    private String name;
+  @NotNull @NotEmpty @Size(max = 255) private String name;
 
-    @NotNull
-    @ISBN
-    private String isbn;
+  @NotNull @ISBN private String isbn;
 
-    @NotNull
-    private Long publisherId;
+  @NotNull private Long publisherId;
 
-    @NotNull
-    private Long authorId;
+  @NotNull private Long authorId;
 
-    @NotNull
-    @Max(3000)
-    private Integer pages;
+  @NotNull @Max(3000) private Integer pages;
 
-    @NotNull
-    @Max(100)
-    private Integer chapters;
+  @NotNull @Max(100) private Integer chapters;
 }
--- src/main/java/com/murilonerdx/bookmanager/model/publisher/dto/PublisherDTO.java
+++ src/main/java/com/murilonerdx/bookmanager/model/publisher/dto/PublisherDTO.java
@@ -1,33 +1,26 @@
 package com.murilonerdx.bookmanager.model.publisher.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.time.LocalDate;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PublisherDTO {
 
-    private Long id;
+  private Long id;
 
-    @NotNull
-    @NotEmpty
-    @Size(max = 255)
-    private String name;
+  @NotNull @NotEmpty @Size(max = 255) private String name;
 
-    @NotNull
-    @NotEmpty
-    @Size(max = 50)
-    private String code;
+  @NotNull @NotEmpty @Size(max = 50) private String code;
 
-    @NotNull
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-    private LocalDate foundationDate;
-
+  @NotNull
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+  private LocalDate foundationDate;
 }
