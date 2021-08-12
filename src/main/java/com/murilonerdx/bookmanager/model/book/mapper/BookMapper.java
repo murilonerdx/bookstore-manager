db966abaa10e6c48ba41fac1441252d0f30b0774
murilonerdx <mu-silva@outlook.com>
2021-08-12 14:23:12 -0300

    Criando todos mappers utilizando struct

---
+++ src/main/java/com/murilonerdx/bookmanager/model/book/mapper/BookMapper.java
@@ -0,0 +1,19 @@
+package com.murilonerdx.bookmanager.model.book.mapper;
+
+import com.murilonerdx.bookmanager.model.book.dto.BookRequestDTO;
+import com.murilonerdx.bookmanager.model.book.dto.BookResponseDTO;
+import com.murilonerdx.bookmanager.model.book.entity.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookMapper {
+
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    Book toModel(BookRequestDTO bookRequestDTO);
+
+    Book toModel(BookResponseDTO bookRequestDTO);
+
+    BookResponseDTO toDTO(Book bookDTO);
+}
---
+++ src/main/java/com/murilonerdx/bookmanager/model/publisher/mapper/PublisherMapper.java
@@ -0,0 +1,16 @@
+package com.murilonerdx.bookmanager.model.publisher.mapper;
+
+import com.murilonerdx.bookmanager.model.publisher.dto.PublisherDTO;
+import com.murilonerdx.bookmanager.model.publisher.entity.Publisher;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PublisherMapper {
+
+    PublisherMapper INSTANCE = Mappers.getMapper(PublisherMapper.class);
+
+    Publisher toModel(PublisherDTO publisherDTO);
+
+    PublisherDTO toDTO(Publisher publisher);
+}
