473fda655d70c645ba11d20952073a0c836dfa7e
Restyled.io <commits@restyled.io>
2021-08-12 16:30:03 +0000

    Restyled by clang-format

--- src/main/java/com/murilonerdx/bookmanager/model/publisher/entity/Publisher.java
+++ src/main/java/com/murilonerdx/bookmanager/model/publisher/entity/Publisher.java
@@ -2,8 +2,8 @@ package com.murilonerdx.bookmanager.model.publisher.entity;
 
 import com.murilonerdx.bookmanager.model.auditable.Auditable;
 import com.murilonerdx.bookmanager.model.book.entity.Book;
-import lombok.Data;
-
+import java.time.LocalDate;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,26 +11,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import java.time.LocalDate;
-import java.util.List;
+import lombok.Data;
 
 @Data
 @Entity
 public class Publisher extends Auditable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String name;
+  @Column(nullable = false, unique = true) private String name;
 
-    @Column(nullable = false, unique = true, length = 100)
-    private String code;
+  @Column(nullable = false, unique = true, length = 100) private String code;
 
-    @Column(nullable = false, columnDefinition = "TIMESTAMP")
-    private LocalDate foundationDate;
+  @Column(nullable = false, columnDefinition = "TIMESTAMP")
+  private LocalDate foundationDate;
 
-    @OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
-    private List<Book> books;
+  @OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
+  private List<Book> books;
 }
