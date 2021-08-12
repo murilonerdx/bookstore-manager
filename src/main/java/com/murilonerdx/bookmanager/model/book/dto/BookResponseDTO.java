4add1fcd720263304965b2baf9f584c2fdea6152
murilonerdx <mu-silva@outlook.com>
2021-08-12 14:10:53 -0300

    Adicionando todos DTOs

---
+++ src/main/java/com/murilonerdx/bookmanager/model/book/dto/BookResponseDTO.java
@@ -0,0 +1,29 @@
+package com.murilonerdx.bookmanager.model.book.dto;
+
+import com.murilonerdx.bookmanager.model.author.dto.AuthorDTO;
+import com.murilonerdx.bookmanager.model.publisher.dto.PublisherDTO;
+import com.rodrigopeleias.bookstoremanager.authors.dto.AuthorDTO;
+import com.rodrigopeleias.bookstoremanager.publishers.dto.PublisherDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookResponseDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String isbn;
+
+    private PublisherDTO publisher;
+
+    private AuthorDTO author;
+
+    private Integer pages;
+
+    private Integer chapters;
+}
