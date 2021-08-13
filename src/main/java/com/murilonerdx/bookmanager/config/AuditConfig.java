c6935069bb2077d388e231e73e978831a0715438
murilonerdx <mu-silva@outlook.com>
2021-08-13 18:51:33 -0300

    Adicionando configurações jwt e web

---
+++ src/main/java/com/murilonerdx/bookmanager/config/AuditConfig.java
@@ -0,0 +1,19 @@
+package com.murilonerdx.bookmanager.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+@EnableTransactionManagement
+public class AuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new BookstoreAuditorAware();
+    }
+}
