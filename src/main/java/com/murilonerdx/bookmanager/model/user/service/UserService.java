b82ec35cec257b1b2a6b9e86546dec64cff3a48d
Restyled.io <commits@restyled.io>
2021-08-13 07:28:35 +0000

    Restyled by clang-format

--- src/main/java/com/murilonerdx/bookmanager/model/user/service/UserService.java
+++ src/main/java/com/murilonerdx/bookmanager/model/user/service/UserService.java
@@ -6,40 +6,39 @@ import com.murilonerdx.bookmanager.model.user.exception.UserNotFoundException;
 import com.murilonerdx.bookmanager.model.user.exceptions.UserAlreadyExistsException;
 import com.murilonerdx.bookmanager.model.user.mapper.UserMapper;
 import com.murilonerdx.bookmanager.model.user.repository.UserRepository;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class UserService {
 
-    private final static UserMapper userMapper = UserMapper.INSTANCE;
+  private final static UserMapper userMapper = UserMapper.INSTANCE;
 
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    public User verifyAndGetUserIfExists(String username) {
-        return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UserNotFoundException(username));
+  public User verifyAndGetUserIfExists(String username) {
+    return userRepository.findByUsername(username).orElseThrow(
+        () -> new UserNotFoundException(username));
+  }
+
+  public void delete(Long id) {
+    verifyAndGetIfExists(id);
+    userRepository.deleteById(id);
+  }
+
+  private User verifyAndGetIfExists(Long id) {
+    return userRepository.findById(id).orElseThrow(
+        () -> new UserNotFoundException(id));
+  }
+
+  private void verifyIfExists(String email, String username) {
+    Optional<User> foundUser =
+        userRepository.findByEmailOrUsername(email, username);
+    if (foundUser.isPresent()) {
+      throw new UserAlreadyExistsException(email, username);
     }
-
-    public void delete(Long id) {
-        verifyAndGetIfExists(id);
-        userRepository.deleteById(id);
-    }
-
-    private User verifyAndGetIfExists(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException(id));
-    }
-
-    private void verifyIfExists(String email, String username) {
-        Optional<User> foundUser = userRepository.findByEmailOrUsername(email, username);
-        if (foundUser.isPresent()) {
-            throw new UserAlreadyExistsException(email, username);
-        }
-    }
-
+  }
 }
